d67bfe69d5b7c1d237cdb888ce8933823282de96
Tom Gordon <thomasgordon02@gmail.com>
2012-11-09 16:02:10 +1100

    JS with ajax + callback. Bootstrap to 2.2.1

--- js/main.js
+++ js/main.js
@@ -16,6 +16,24 @@
         appendP: function () {
             console.log('appendP has run');
             $('.wrapper').append('<p>appendP function is running.</p>');
+
+            this.ajaxExample(function (data) {
+
+            })
+        },
+
+        ajaxExample: function (callback) {
+            $.ajax({
+                method: "POST",
+                url: "#",
+                data: {
+                    thing: 1,
+                    thing2: 2
+                },
+                success: function (data) {
+                    callback(data);
+                }
+            })
         }
     };
 
